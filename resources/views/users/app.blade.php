c2b0b1a43d675b38f2dc6d29ce17b649c055132c
dahalbishwas <dahalbishwas11@gmail.com>
2018-06-07 13:27:35 +0545

    test checkin

---
+++ resources/views/users/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+  @include ('users/layouts/head')
+
+  </head>
+
+  <body>
+
+    @include('users/layouts/header')
+    <hr>
+      @section('main-content')
+
+
+
+      @show
+
+
+
+  @include ('users/layouts/footer')
+
+  </body>
+
+</html>
